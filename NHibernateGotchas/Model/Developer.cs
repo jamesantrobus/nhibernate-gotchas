ab4c23ba8829d77ec987fa379173cb2c728c4505
James Antrobus <jamesantrobus@gmail.com>
2013-02-11 21:54:24 +0000

    working demo of default values producing false positives in persistence specifiction

--- NHibernateGotchas/Model/Developer.cs
+++ NHibernateGotchas/Model/Developer.cs
@@ -5,5 +5,8 @@
 		public virtual int Id { get; set; }
 		public virtual string Name { get; set; }
 		public virtual WebFramework Framework { get; set; }
+		
+		// unmapped property
+		public virtual bool IsActive { get; set; }
 	}
 }
---
+++ NHibernateGotchas/Tests/PerilsOfDefaultValues.cs
@@ -0,0 +1,27 @@
+﻿using FluentNHibernate.Testing;
+using NHibernateGotchas.Model;
+using NUnit.Framework;
+
+namespace NHibernateGotchas.Tests
+{
+	public class PerilsOfDefaultValues : TestBase
+	{
+		// example: the boolean IsActive property is not mapped
+
+		// testing the property with a default value will cause the persistence specification to pass (incorrect)
+
+		// testing with a non-default value will (correctly) cause the test to fail
+
+		[Test]
+		[TestCase(false, Description = "False positive")]
+		[TestCase(true, Description = "Failing test (correct)")]
+		public void VerifyIsActiveFieldWithoutMappingTheColumn(bool isActive)
+		{
+			new PersistenceSpecification<Developer>(Session)
+				.CheckProperty(x => x.Name, "James")
+				.CheckProperty(x => x.Framework, WebFramework.Django)
+				.CheckProperty(x => x.IsActive, isActive)
+				.VerifyTheMappings();
+		}
+	}
+}
